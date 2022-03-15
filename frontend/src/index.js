9c9514ddea87ee41d359f4149b8146baccde4f50
okdongdong <kjch6411@naver.com>
2022-03-15 23:30:59 +0900

    #S06P22E201-48 feat
    초기환경세팅

---
+++ frontend/src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import appReducer from "./reducers";
+
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+
+const store = createStore(appReducer);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
